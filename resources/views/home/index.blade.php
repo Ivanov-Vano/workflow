8e272e234bd935bfd4c1de3dce381af579d8f0ec
lin071 <linyushin85@gmail.com>
2022-04-06 17:56:20 +0300

    First commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.main')
+@section('content')
+    <div class="page-container">
+        <div class="page-content-wrapper animated fadeInRight">
+            <div class="page-content">
+                <div class="row wrapper border-bottom page-heading">
+                     <div class="panel-heading text-center"><h2>Главная панель</h2></div>
+                    <div class="wrapper-content">
+                            <i class="fas fa-columns"></i> Выбрать отображаемые колонки
+                        <div class=" pull-right">
+                            <a class="btn btn-sm blue" role="button" data-toggle="collapse" href="#filter-form" aria-expanded="false" aria-controls="filter-form">
+                                <span class="glyphicon glyphicon-filter"></span>
+                            </a>
+                            <button class="btn btn-sm green" id="deselect-all">Убрать выделение</button>
+                            <button class="btn btn-sm blue" id="select-all">Выделить все</button>
+                            <button class="btn btn-sm red" id="multiple-delete">Удалить выделенные</button>
+                        </div>
+                        <div class="row">
+                            <table id="indocs-table" class="table table-bordered table-hover display">
+                                <thead>
+                                <tr>
+                                    <th class="font-bold text-center"></th>
+                                    <th></th>
+                                    <th>Вх. №<br> (при наличии)</th>
+                                    <th>Дата Вх. № </th>
+                                    <th>Исх. №</th>
+                                    <th>Дата Исх. №</th>
+                                    <th>Отправитель</th>
+                                    <th>Содержание документа</th>
+                                </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
